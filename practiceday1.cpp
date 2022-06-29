46fdb85ef8633098a2707037922abf6d28c34e33
Sauru6527 <89717054+Sauru6527@users.noreply.github.com>
2022-06-29 23:25:47 +0530

    Add files via upload

---
+++ practiceday1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<conio.h>
+#include<fstream>
+class Start
+{
+ public
+ 
+	 
+	      int num =0;
+    
+    
+  /* int takeinput
+    {
+    	cout<<"ENTER THE NUMBER ">>num<<endl;
+	}
+	
+    void display
+    {
+      	
+    cout<<"THE OUTPUT IS = "<<endl;
+	}*/
+ 
+};
+
+int main()
+{
+	//Start obj;
+	cout<<"CLASS CREATED AND OBJ CALLED.............."<<endl;
+	
+	
+	//obj.takeinput();
+	cout<<"FUNCTION TAKE INPUT CALLED............"<<endl;
+	
+	//obj.display();
+	cout<<"FUNCTION DISPLAY CALLED..........."<<endl;
+	
+	return 0;
+}
+
